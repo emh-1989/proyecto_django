d62a7ac0ec2240515cace83e6edb84f51c0b003a
Emmanuel Hidalgo <hidalgo_emmanuel@hotmail.com>
2025-04-11 11:18:36 -0300

    configuré pagina admin, implementé depend on para corregir error de conexion

--- src/django/Proyecto_curza_2025/settings.py
+++ src/django/Proyecto_curza_2025/settings.py
@@ -74,17 +74,23 @@ WSGI_APPLICATION = 'Proyecto_curza_2025.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME': os.environ.get('POSTGRES_DB'),
+    #    'USER': os.environ.get('POSTGRES_USER'),
+     #   'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+      #  'HOST': os.environ.get('DB_HOST'),
+       # 'PORT': '5432',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- src/django/aulas/models.py
+++ src/django/aulas/models.py
@@ -11,9 +11,21 @@ class Musician(models.Model):
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
     
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.instrument})"
+    
+    def __unicode__(self):
+        return f"{self.first_name} {self.last_name} ({self.instrument})"
+
 
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_stars = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.artist})"
+    
+    def __unicode__(self):
+        return f"{self.name} ({self.artist})"
