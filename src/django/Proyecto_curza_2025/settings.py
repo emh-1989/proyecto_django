89b632c354589e51b6738b7e047d9944bdf69cbe
Emmanuel Hidalgo <hidalgo_emmanuel@hotmail.com>
2025-04-10 22:26:57 -0300

    configurando postgres, me tira error porque el backend intenta conectarse antes que se haya iniciado la BS

--- src/django/Proyecto_curza_2025/settings.py
+++ src/django/Proyecto_curza_2025/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Proyecto_curza_2025.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
